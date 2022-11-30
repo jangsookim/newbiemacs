dcc7592a057b7ec627c57b21959a5d3812fafd6d
jangsookim <jangsookim@skku.edu>
2022-11-30 14:50:26 +0900

    Add nbm-update-version

--- nbm-system/nbm-devel.el
+++ nbm-system/nbm-devel.el
@@ -13,3 +13,17 @@
 	(save-buffer) (kill-buffer)
 	(message "nbm-home.txt created. (contents:~/Dropbox/newbiemacs)")
 	))))
+
+(defun nbm-update-version ()
+  "Update Newbiemacs version."
+  (interactive)
+  (find-file (nbm-root-f "nbm-system/nbm-newbie-mode.el"))
+  (beginning-of-buffer)
+  (re-search-forward "\\(\"Newbiemacs \\)\\([0-9]+[.][0-9]+\\)")
+  (replace-match (read-string "Enter new version: ")
+		 nil nil nil 2)
+  (if (equal ?y (read-char "Is this okay? (y or n): "))
+      (save-buffer) (revert-buffer nil t))
+  (kill-buffer))
+
+
