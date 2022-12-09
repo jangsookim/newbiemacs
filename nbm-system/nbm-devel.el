92972789eceb2ce3344d6b68956a6e11f7b7c7cf
jangsookim <jangsookim@skku.edu>
2022-12-09 14:31:25 +0900

    Add nbm-toggle-pdf-viewer

--- nbm-system/nbm-devel.el
+++ nbm-system/nbm-devel.el
@@ -25,3 +25,31 @@
   (if (equal ?y (read-char "Is this okay? (y or n): "))
       (save-buffer) (revert-buffer nil t))
   (kill-buffer))
+
+(defun nbm-toggle-pdf-viewer ()
+  (interactive)
+  (if (equal (nth 1 (car TeX-view-program-selection)) "PDF Tools")
+      (progn
+	(setq TeX-view-program-list
+	      '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o"))
+	      TeX-view-program-selection '((output-pdf "Skim")))
+	(setq openwith-associations '(("\\.pdf\\'" "open" (file))
+				      ("\\.hwp\\'" "open" (file))
+				      ("\\.xlsx\\'" "open" (file))
+				      ("\\.djvu\\'" "/Applications/DjView.app/Contents/bin/djview" (file))))
+	)
+    (progn
+      (pdf-tools-install)
+      (setq auto-revert-interval 0.1)
+      (global-auto-revert-mode)
+      (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+	    TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
+	    TeX-source-correlate-start-server t)
+      (setq openwith-associations '(
+				    ("\\.hwp\\'" "open" (file))
+				    ("\\.xlsx\\'" "open" (file))
+				    ))
+      )
+    )
+  )
+
