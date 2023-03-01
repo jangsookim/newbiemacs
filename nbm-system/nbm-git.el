27f2393fe94ab0fea2d9ea20c2c770061827567a
jangsookim <jangsookim@skku.edu>
2023-03-01 14:31:07 +0900

    Update nbm-git-init and nbm-git-publish-to-github for more features

--- nbm-system/nbm-git.el
+++ nbm-system/nbm-git.el
@@ -1,41 +1,50 @@
 (defun nbm-git-init ()
-  "Initiate git repository. Ask if clone it to GitHub."
+  "Initiate a git repository. Ask if publish it to GitHub."
   (interactive)
-  (when (equal ?y (read-char (format "Create a new git repository from the current directory? (type y or n)
+  (let (warning)
+    (if (file-exists-p "./.git")
+	(message "The current directory is already a git repository.")
+      (when (equal ?y (read-char (format "Create a new git repository from the current directory? (type y or n)
 Current directory: %s" (nbm-get-dir-name))))
-    (shell-command "git init")
-    (when (equal ?y (read-char "Do you want to publish the current repository to GitHub? (Type y for yes): "))
-      (nbm-git-publish-to-github))))
+	(setq warning (shell-command-to-string "git init --quiet"))
+	(if (not (equal warning ""))
+	    (message warning)
+	  (when (equal ?y (read-char "Do you want to publish the current repository to GitHub? (type y for yes): "))
+	    (nbm-git-publish-to-github)))))))
 
 (defun nbm-git-publish-to-github ()
   "Publish the current git repository to github."
   (interactive)
-  (let (confirm)
-    (let (repo-name choice access username gh status)
-      (if (file-exists-p "/opt/homebrew/bin/gh")
-	  (setq gh "/opt/homebrew/bin/gh") (setq gh "gh"))
-      (setq status (shell-command-to-string "gh auth status"))
-      (string-match "Logged in to github.com as \\([^ ]+\\) " status)
-      (setq username (match-string 1 status))
-      (unless username 
-	(message "Failed to connect to GitHub.
+  (let (remote repo-name access username gh status)
+    (setq remote (shell-command-to-string "git remote -v"))
+    (if (not (equal remote ""))
+	(message (format "There is an existing remote repository.\n%s" remote))
+      (progn
+	(if (file-exists-p "/opt/homebrew/bin/gh")
+	    (setq gh "/opt/homebrew/bin/gh") (setq gh "gh"))
+	(setq status (shell-command-to-string "gh auth status"))
+	(string-match "Logged in to github.com as \\([^ ]+\\) " status)
+	(setq username (match-string 1 status))
+	(unless username 
+	  (message "Failed to connect to GitHub.
 Make sure that you have installed GitHub CLI and run the following command in a terminal.
 gh auth login"))
-      (when username
-	(setq repo-name (read-string (concat "Enter a name for the new GitHub repository: ")
-				     (file-name-nondirectory (directory-file-name (nbm-get-dir-name)))))
-	(setq repo-name (string-replace " " "-" repo-name))
-	(setq access (completing-read "Choose the accessibility: " '("private" "public")))
-	(shell-command (format "%s repo create %s --%s" gh repo-name access))
-	(shell-command (format "git remote add origin https://github.com/%s/%s.git" username repo-name))
-	(shell-command "git branch -M main")
-	(shell-command "git push -u origin main")
-	(if (string-search (format "From https://github.com/%s/%s.git" username repo-name)
-			   (shell-command-to-string "git ls-remote --exit-code"))
-	    (message (format "The following GitHub repository has been created.
+	(when username
+	  (setq repo-name (read-string (concat "Enter a name for the new GitHub repository: ")
+				       (file-name-nondirectory (directory-file-name (nbm-get-dir-name)))))
+	  (setq repo-name (string-replace " " "-" repo-name))
+	  (setq access (completing-read "Choose the accessibility: " '("private" "public")))
+	  (shell-command (format "%s repo create %s --%s" gh repo-name access))
+	  (shell-command (format "git remote add origin https://github.com/%s/%s.git" username repo-name))
+	  (shell-command "git branch -M main")
+	  (unless (string-match "^No commits yet$" (shell-command-to-string "git status"))
+	    (shell-command "git push -u origin main"))
+	  (if (string-search (format "From https://github.com/%s/%s.git" username repo-name)
+			     (shell-command-to-string "git ls-remote --exit-code"))
+	      (message (format "The following GitHub repository has been created.
 https://github.com/%s/%s.git" username repo-name))
-	  (message (format "Failed to create the following GitHub repositiory.
-https://github.com/%s/%s.git" username repo-name)))))))
+	    (message (format "Failed to create the following GitHub repositiory.
+https://github.com/%s/%s.git" username repo-name))))))))
 
 (defun nbm-git-merge ()
   "Run a simple git merge tool in the current file."
