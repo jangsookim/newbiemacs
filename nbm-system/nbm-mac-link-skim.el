801b03c1757a82710d1346b7ed4e4a7106fbe39e
jangsookim <jangsookim@skku.edu>
2024-02-17 13:46:10 +0900

    Add nbm-skim-bookmark-jump

--- nbm-system/nbm-mac-link-skim.el
+++ nbm-system/nbm-mac-link-skim.el
@@ -56,41 +56,41 @@ This variable is then saved in newbiemacs/nbm-user-settings/nbm-variables/nbm-sk
 (defun nbm-skim-goto-page-run ()
   "Go to a specific page of the pdf current opened in skim."
   (interactive)
-  (nbm-skim-goto-page (read-string "Page (write + or - for offset): ")))
+  (let (path)
+    (setq path (nbm-skim-get-filename))
+    (if path
+	(nbm-skim-goto-page path (read-string "Page (write + or - for offset): "))
+      (message "You must open a pdf in Skim."))))
 
-(defun nbm-skim-goto-page (page &optional offset)
-  "Go to a specific page of the pdf current opened in skim.
+(defun nbm-skim-goto-page (path page &optional offset)
+  "Go to a specific page of the pdf in PATH.
 PAGE must be a string of the form +num, -num, or num. For example, +12, -4, or 100."
   (nbm-skim-data-init)
-  (let (str)
-    (setq str (nbm-skim-get-filename))
-    (unless str
-      (message "You must open a pdf in Skim."))
-    (when str
-      (if (member (substring page 0 1) '("+" "-"))
-	  (nbm-skim-set-page-offset (string-to-number page))
-	(progn
-	  (unless offset
-	    (setq offset (nbm-skim-data-get (file-name-nondirectory str)))
-	    (unless offset (setq offset "0")))
-	  (setq str (format "%s::%s" str (+ (string-to-number page) (string-to-number offset))))
-	  (org-mac-link-skim-open str nil))))))
+  (if (member (substring page 0 1) '("+" "-"))
+      (nbm-skim-set-page-offset (string-to-number page))
+    (progn
+      (unless offset
+	(setq offset (nbm-skim-data-get (file-name-nondirectory path)))
+	(unless offset (setq offset "0")))
+      (setq path (format "%s::%s" path (+ (string-to-number page) (string-to-number offset))))
+      (org-mac-link-skim-open path nil))))
 
 (defun nbm-skim-bookmark-jump ()
-  "Jumpt to a bookmark of the pdf current opened in skim."
+  "Jump to a bookmark of the pdf current opened in skim."
   (interactive)
-  (let (page str bookmark offset txt)
+  (let (page str bookmark offset txt path)
     (unless (file-exists-p (nbm-f "nbm-user-settings/nbm-variables/nbm-skim-bookmarks"))
       (make-directory (nbm-f "nbm-user-settings/nbm-variables/nbm-skim-bookmarks")))
-    (setq str (nbm-skim-get-filename))
-    (unless str
-      (message "You must open a pdf in Skim."))
-    (when str
-      (setq str (file-name-sans-extension (file-name-nondirectory str)))
+    (setq path (nbm-skim-get-filename))
+    (unless path (nbm-skim-open-bookmark-file))
+    (when path
+      (setq str (file-name-sans-extension (file-name-nondirectory path)))
       (setq txt (nbm-f (format "nbm-user-settings/nbm-variables/nbm-skim-bookmarks/%s.txt" str)))
       (unless (file-exists-p txt)
-	(find-file txt)
-	(insert "; Any line starting with a semi-colon will be ignored.
+	(when (equal ?y (read-char "The current pdf has no bookmark. Do you want to create a bookmark? (Type y for yes.): "))
+	  (find-file txt)
+	  (insert (format "; (DO NOT DELETE THIS LINE!) path to file: %s\n" path))
+	  (insert "; Any line starting with a semi-colon will be ignored.
 ; Each line must be something like description page number as follows.
 ; 5.5 Exponential structures 45
 ; Selecting the above line will make Skim jump to page 45 with proper offset of the current pdf.
@@ -106,18 +106,35 @@ PAGE must be a string of the form +num, -num, or num. For example, +12, -4, or 1
 ; 1.2 Sets and multisets 23
 ; 1.3 Cycles and inversions 29
 ; 1.4 Descents 38\n\n")
-	(save-buffer) (kill-buffer))
-      (setq bookmark (split-string (f-read-text txt) "\n"))
-      (setq bookmark (cl-remove-if (lambda (x) (or (length= x 0) (equal (substring x 0 1) ";"))) bookmark))
-      (setq bookmark (cons "Select this if you want to edit the bookmark file." bookmark))
-      (setq page (completing-read "Where to? " bookmark))
-      (if (equal page (car bookmark))
-	  (find-file txt)
-	(progn
-	  (while (equal (substring page -1 nil) " ")
-	    (setq page (substring page 0 -1)))
-	  (setq page (car (last (split-string page " "))))
-	  (when (equal (substring page 0 1) "x")
-	    (setq page (substring page 1 nil))
-	    (setq offset "0"))
-	  (nbm-skim-goto-page page offset))))))
+	  (save-buffer) (kill-buffer)))
+      (when (file-exists-p txt)
+	(setq bookmark (split-string (f-read-text txt) "\n"))
+	(setq bookmark (cl-remove-if (lambda (x) (or (length= x 0) (equal (substring x 0 1) ";"))) bookmark))
+	(setq bookmark (cons "Select this if you want to edit the bookmark file." bookmark))
+	(setq page (completing-read "Where to? " bookmark))
+	(if (equal page (car bookmark))
+	    (find-file txt)
+	  (progn
+	    (while (equal (substring page -1 nil) " ")
+	      (setq page (substring page 0 -1)))
+	    (setq page (car (last (split-string page " "))))
+	    (when (equal (substring page 0 1) "x")
+	      (setq page (substring page 1 nil))
+	      (setq offset "0"))
+	    (nbm-skim-goto-page path page offset)))))))
+
+(defun nbm-skim-open-bookmark-file ()
+  "Open a bookmark file listed in the following directory.
+newbiemacs/nbm-user-settings/nbm-variables/nbm-skim-bookmarks"
+  (interactive)
+  (let (files file txt beg end path)
+    (dolist (file (directory-files (nbm-f "nbm-user-settings/nbm-variables/nbm-skim-bookmarks")))
+      (unless (or (equal (substring file 0 1) ".") (equal (substring file -1 nil) "~"))
+	(setq files (nbm-append (file-name-sans-extension file) files))))
+    (setq txt (completing-read "Open which file? " files))
+    (setq txt (nbm-f (format "nbm-user-settings/nbm-variables/nbm-skim-bookmarks/%s.txt" txt)))
+    (find-file txt) (beginning-of-buffer)
+    (search-forward "path to file: ")
+    (setq beg (point)) (end-of-line) (setq end (point))
+    (setq path (buffer-substring beg end))
+    (kill-buffer) (find-file path)))
