d7f8fc986b3e3f523b71ab9cefbf115f7b18f5ee
jangsookim <jangsookim@skku.edu>
2024-03-08 17:22:49 +0900

    Fix bug in nbm-arxiv-download-pdf

--- nbm-system/nbm-arxiv.el
+++ nbm-system/nbm-arxiv.el
@@ -2,10 +2,11 @@
 
 (defun nbm-arxiv-download-pdf (url)
   "Download the pdf file for the paper in the URL of an arXiv abstract page."
-  (let (id pdf)
+  (let (id pdf id-without-slash)
     (setq id (biblio-arxiv--extract-id url))
+    (setq id-without-slash (string-replace "/" "-" id))
     (setq pdf (biblio-arxiv--pdf-url id))
-    (url-copy-file pdf (format "%s/%s.pdf" *nbm-downloads* id) t)))
+    (url-copy-file pdf (format "%s/%s.pdf" *nbm-downloads* id-without-slash) t)))
 
 (defun nbm-arxiv-get-bibtex (url)
   "Get the bibtex file from url."
@@ -30,7 +31,7 @@ The URL of an arxiv abstract page must be copied or
 	(progn
 	  (setq id (biblio-arxiv--extract-id url))
 	  (setq pdf (biblio-arxiv--pdf-url id))
-	  (url-copy-file pdf (format "%s/%s.pdf" *nbm-downloads* id))
+	  ;; (url-copy-file pdf (format "%s/%s.pdf" *nbm-downloads* id))
 	  (nbm-arxiv-download-pdf url)
 	  (kill-new (nbm-arxiv-get-bibtex url))
 	  (nbm-move-pdf-from-downloads))
