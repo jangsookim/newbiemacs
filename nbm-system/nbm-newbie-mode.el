00e11daf288a894afb709cd16e1e6a564892094f
jangsookim <jangsookim@skku.edu>
2025-05-25 22:26:35 +0900

    Override with-editor-cancel keybinding

--- nbm-system/nbm-newbie-mode.el
+++ nbm-system/nbm-newbie-mode.el
@@ -98,7 +98,7 @@
 (defun newbie-print-logo ()
   "Start newbie."
   (nbm-insert 7 "                                       +-+                        ")
-  (insert (format " %s \n" "Newbiemacs 1.93"))
+  (insert (format " %s \n" "Newbiemacs 1.94"))
   (nbm-insert 7 "                                       | | +----;                                  
                                +----,  | | | +--' +-+    +-+                       
   +-+    +-+                   | ,_, \\ | | | |    |  \\  /  |                       
