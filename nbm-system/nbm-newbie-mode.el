48eb7792c6948a61f5bb44f89b984d83240b8828
jangsookim <jangsookim@jangsookimui-MacBookPro.local>
2026-05-04 15:25:07 +0900

    Newbiemacs 2.23

--- nbm-system/nbm-newbie-mode.el
+++ nbm-system/nbm-newbie-mode.el
@@ -98,7 +98,7 @@
 (defun newbie-print-logo ()
   "Start newbie."
   (nbm-insert 7 "                                       +-+                        ")
-  (insert (format " %s \n" "Newbiemacs 2.22"))
+  (insert (format " %s \n" "Newbiemacs 2.23"))
   (nbm-insert 7 "                                       | | +----;                                  
                                +----,  | | | +--' +-+    +-+                       
   +-+    +-+                   | ,_, \\ | | | |    |  \\  /  |                       
