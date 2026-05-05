e0528b64aba4742912be2ab95adf9dd038290dce
jangsookim <jangsookim@skku.edu>
2026-05-06 08:05:49 +0900

    Newbiemacs 2.25

--- nbm-system/nbm-newbie-mode.el
+++ nbm-system/nbm-newbie-mode.el
@@ -98,7 +98,7 @@
 (defun newbie-print-logo ()
   "Start newbie."
   (nbm-insert 7 "                                       +-+                        ")
-  (insert (format " %s \n" "Newbiemacs 2.24"))
+  (insert (format " %s \n" "Newbiemacs 2.25"))
   (nbm-insert 7 "                                       | | +----;                                  
                                +----,  | | | +--' +-+    +-+                       
   +-+    +-+                   | ,_, \\ | | | |    |  \\  /  |                       
